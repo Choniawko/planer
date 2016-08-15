3f77d61f7ba5542d5accaa1685f3f382f1c6778b
Ola Rybacka <rybacka.ola@gmail.com>
2016-08-15 16:54:38 +0200

    user add with validation

--- src/app/users/components/user-edit.ts
+++ src/app/users/components/user-edit.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { FORM_DIRECTIVES } from '@angular/common';
+
 
 import { UserService } from '../user.service';
 import { User } from '../user.model'; 
@@ -7,25 +10,48 @@ import { User } from '../user.model';
 @Component({
     selector: 'user-edit',
     templateUrl: 'user-edit.html',
-    providers: [UserService]
+    providers: [UserService],
+    directives: [FORM_DIRECTIVES]
 })
 export class UserEdit implements OnInit {
+
     public errorMessage: string;
+    public finished = false;
+    newUser : User = new User('');
+    public passwordsEqual = false;
+    public passwordsNotEqual = false;
 
     constructor(public userService: UserService) { }
 
     ngOnInit() {     
-        this.saveUser();
+        
+    }
+
+    arePasswordsEqual(pass2){
+        if(pass2 == this.newUser.password){
+            this.passwordsEqual = true;
+            this.passwordsNotEqual = false;
+        } else {
+            this.passwordsNotEqual = true;
+            this.passwordsEqual = false;
+        }
     }
     
+   
     saveUser(){
-        var user = new User({name: "Imie", password: "haslo", admin: true});
-        this.userService.saveUser(user)
+        this.newUser.admin = false;
+        this.userService.saveUser(this.newUser)
         .subscribe(
             data => {
                 console.log("success")
             }, 
             err => console.log('fail')
         )
+    }
+
+    onSubmit(){
+        this.finished = true;
+        console.log(this.newUser);
+        this.saveUser();
     }
 }
