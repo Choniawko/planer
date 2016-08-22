9a4afc3f91d559ac35b01e88670e166c570ccdf2
choniawko <pawel.choniawko@gmail.com>
2016-08-22 18:07:26 +0200

    config mongo

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,7 @@
-var dbConUrl = process.env.MONGOLAB_URI|| 'mongodb://localhost:27017/test';
-
+var mongoURI = process.env.MONGOLAB_BROWN_URI || "mongodb://heroku_vj4mnbbn:udev0r2fukkdfq1gbm9igmtrtn@ds063725.mlab.com:63725/heroku_vj4mnbbn"
 module.exports = {
 
     'secret': 'secretpassword',
-    'database': dbConUrl
+    'database': mongoURI
 
 };
